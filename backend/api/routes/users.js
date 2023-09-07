2ca3f61809e2fa0037def1ab6239597a37cc151a
luqmanity <luqman.yazrie@gmail.com>
2023-09-07 17:46:02 +0800

    A commitment to be commited for

--- backend/api/routes/users.js
+++ backend/api/routes/users.js
@@ -3,13 +3,14 @@ const db = require('../../functions/database');
 const database = require('../../functions/database');
 const router = express.Router();
 
+// Get request
 router.get('/', (req, res, next) => {
     res.status(200).json({
         message: "Users were fetched"
     });
 });
 
-// Adding new product
+// Adding new users
 router.post('/', (req, res, next) => {
     const user = {
         email: req.body.email,
@@ -17,7 +18,6 @@ router.post('/', (req, res, next) => {
         lastname: req.body.lastname,
         age: req.body.age
     };
-    console.log(user);
     database(`
     INSERT INTO users (email, firstname, lastname, age)
     VALUES ('${user.email}', '${user.firstname}', '${user.lastname}', ${user.age});
